6ebade80c91e0f49324cd8238feab15b63b1ba70
Ofir <ofirgruner@gmail.com>
2018-11-27 13:10:25 +0200

    Posting addition

---
+++ src/main/java/Model/TermsEntranceRow.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class TermsEntranceRow extends AEntranceRow {
+
+    public TermsEntranceRow(String docNo, String fileName, int termFreqInDoc) {
+        super(docNo, fileName, termFreqInDoc);
+
+    }
+}
+
