d0bfd9320ca6021fa320b0fde03a254c36d3053e
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-01-02 00:07:08 +0200

    Removed unused functions

--- src/main/java/Model/SecondPart/SecondPartModel.java
+++ src/main/java/Model/SecondPart/SecondPartModel.java
@@ -10,7 +10,6 @@ public class SecondPartModel {
     private TotalDictionaryController totalDictionaryController;
     private HashSet<String> stopWords;
     private boolean toStem;
-    HashMap<String, List<String>> qrels;
 
     public void LoadDictionary(TotalDictionaryController totalDictionaryController, boolean isStemmed){
         this.totalDictionaryController = totalDictionaryController;
@@ -33,100 +32,6 @@ public class SecondPartModel {
         System.out.println("Finished searching in a bunch of Queries");
         long time = stopWatch.getTime();
         System.out.println("Total time = " + time + " milliseconds");
-
-        /*
-        List<List<DocumentsDictionaryEntrance>> data = new ArrayList<>();
-        for(MyQuery query : queries){
-            data.add(query.getRetrievedDocuments());
-        }
-        */
-        //CountRelevantDocuments(queries);
-
-        //return data;
-    }
-
-    public List<DocumentsDictionaryEntrance> Search(MyQuery query, boolean useSemantics){
-        StopWatch stopWatch = new StopWatch();
-        System.out.println("Starting to search");
-        stopWatch.start();
-
-        Searcher searcher = new Searcher(stopWords,toStem,totalDictionaryController, useSemantics);
-        List<MyQuery> queries = new ArrayList<>(1);
-        queries.add(query);
-        searcher.SearchForRelevantDocuments(queries);
-
-        stopWatch.stop();
-        System.out.println("Finished searching");
-        long time = stopWatch.getTime();
-        System.out.println("Total time = " + time + " milliseconds");
-
-        //CountRelevantDocuments(query.getRetrievedDocuments(),query.getId());
-        return query.getRetrievedDocuments();
-    }
-
-    public void CountRelevantDocuments(List<DocumentsDictionaryEntrance> result, String qID){
-        int count = 0;
-        List<String> relevant = qrels.get(qID);
-        for(DocumentsDictionaryEntrance entrance : result){
-            String docid = entrance.getDocNo().replace(" ", "");
-            if(relevant.contains(docid))
-                count++;
-        }
-        System.out.println("relevant doc number = " + count);
-        System.out.println("total amount of relevant docs = " + relevant.size());
-    }
-
-    public void CountRelevantDocuments(List<MyQuery> queries){
-        int count = 0;
-        int relevantCount = 0;
-        for(MyQuery query : queries) {
-            int currCount = 0;
-            int currRelevantCount = 0;
-            List<DocumentsDictionaryEntrance> documents = query.getRetrievedDocuments();
-            List<String> relevant = qrels.get(query.getId());
-            relevantCount += relevant.size();
-            currRelevantCount = relevant.size();
-            for (DocumentsDictionaryEntrance entrance : documents) {
-                String docid = entrance.getDocNo().replace(" ", "");
-                if (relevant.contains(docid)) {
-                    count++;
-                    currCount++;
-                }
-            }
-
-            System.out.println("relevant doc number for curr query = " + currCount);
-            System.out.println("amount of relevant docs for curr query = " + currRelevantCount);
-        }
-        System.out.println();
-        System.out.println();
-        System.out.println("total relevant doc number = " + count);
-        System.out.println("total amount of relevant docs = " + relevantCount);
-    }
-
-    public void LoadQrels(File qrels){
-        try {
-            this.qrels = new HashMap<>();
-            Scanner scanner = new Scanner(new BufferedReader(new FileReader(qrels)));
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                String[] split = line.split(" ");
-                String id = split[0];
-                String doc = split[2];
-                int rel = Integer.parseInt(split[3]);
-                if(rel == 1)
-                {
-                    if(this.qrels.containsKey(id)){
-                        this.qrels.get(id).add(doc);
-                    }
-                    else{
-                        this.qrels.put(id,new ArrayList<>());
-                        this.qrels.get(id).add(doc);
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     public List<MyQuery> ReadQueriesFile(File queriesFile) {
